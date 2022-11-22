26a64f858fecefa266ef6a29399b3b219a6b012e
zohaibhn <maakzrsmart5@gmail.com>
2022-11-22 13:30:22 -0500

    Fixed problem preventing algorithm from being implemented

--- src/Main.java
+++ src/Main.java
@@ -10,13 +10,11 @@ public class Main {
     static ArrayList<String> ordering;
 
     public static boolean noEdge(String name) {
-        boolean result = false;
+        boolean result = true;
         int num = graph.lookup(name);
         for (int i = 0; i < graph.getSize(); i++) {
             if (graph.isEdge(i, num) == true) {
-                result = false;
-            } else {
-                result = true;
+                return false;
             }
         }
         return result;
@@ -29,38 +27,45 @@ public class Main {
         ordering = new ArrayList<String>();
         File fi = new File("src/" + name);
         try {
-            FileInputStream file = new FileInputStream(fi);
-            FileInputStream fileTwo = new FileInputStream(fi);
-            Scanner scan = new Scanner(file);
-            Scanner secondScan = new Scanner(fileTwo);
-            int graphNum = Integer.parseInt(scan.nextLine());
-            secondScan.nextLine();
-            graph = new Graph<>(graphNum);
-            System.out.println(graphNum);
+            FileInputStream file = new FileInputStream(fi); //first input stream to read file
+            FileInputStream fileTwo = new FileInputStream(fi); //second input stream to read file
+            Scanner scan = new Scanner(file); //scanner to read file the first time
+            Scanner secondScan = new Scanner(fileTwo); //Scanner to read file the second time
+            int graphNum = Integer.parseInt(scan.nextLine()); //reads number to create graph size
+            secondScan.nextLine(); //skips that number in the second scanner
+            graph = new Graph<>(graphNum); //create the graph with the size read from file
+            System.out.println(graphNum); //test case
             int i = 0;
-            while(scan.hasNext()) {
+            while(scan.hasNext()) { //read file with first scanner
 
-                String line = scan.next();
+                String line = scan.next(); //reads first class name
                 //System.out.println(line + " " + i);
-                graph.setValue(i, line);
-                scan.nextLine();
+                graph.setValue(i, line); //create a node with class
+                scan.nextLine(); //goes to next line
                 i++;
             }
-            while(secondScan.hasNextLine()) {
-                String firstClass = secondScan.next();
-                //System.out.println(firstClass);
-                int loop = secondScan.nextInt();
-                    for (int j = 0; j < loop; j++) {
-                        graph.insertEdge(graph.lookup(secondScan.next()), graph.lookup(firstClass));
-                        //System.out.println(secondScan.next() + " LOOPER");
+            while(secondScan.hasNextLine()) { //Second scanner to read the file to assign edges
+                String firstClass = secondScan.next(); //reads first class in line
+                //System.out.println(firstClass + " TESTTTT"); //test
+                int loop = secondScan.nextInt(); //reads number of prerequisites following the first class name
+                    for (int j = 0; j < loop; j++) { //loop to go through each number of prerequisites
+                        String secondClass = secondScan.next(); //reads class that is prerequisite
+                        //System.out.println(firstClass + " FIRST");
+                        //System.out.println(secondClass + " SECONG");
+                         graph.insertEdge(graph.lookup(secondClass), graph.lookup(firstClass));
                     }
             }
+            //for (int j = 0; j < graph.getSize(); j++) {
+            //    System.out.println(noEdge(String.valueOf(graph.getValue(j))) + " EDGE TEST");
+            //}
             for (int j = 0; j < graph.getSize(); j++) {
                 //System.out.println(graph.getValue(j) + "GGG");
                 if (noEdge(String.valueOf(graph.getValue(j))) == true) {
                     active.add(String.valueOf(graph.getValue(j)));
                 }
             }
+            System.out.println(active.size() + " SIZE");
+
             while (!active.isEmpty()) {
                 ordering.add(active.get(0));
                 //System.out.println(active.get(0));
@@ -74,6 +79,7 @@ public class Main {
                 }
                 active.remove(0);
             }
+
             for (int j = 0; j < ordering.size(); j++) {
                 //System.out.println(ordering.get(i));
             }
